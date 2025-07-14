3765ebf244ec06fc9facfbd8eaded750e31afe0f
Srilaxmi nampally <srilaxmihya@gmail.com>
2025-07-14 15:33:24 +0530

    my first commit in main

---
+++ App.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    username=request.form['username']
+    rollno=request.form['rollno']
+    email=request.form['email']
+    year=request.form['year']
+    return render_template('result.html',name=username,roll=rollno,mail=email,yr=year)
+if __name__=='__main__':
+    app.run(debug=True)
